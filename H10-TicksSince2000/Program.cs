634fe98837066a02b94578f775679f3e15bc30c0
Ysa-04 <ysaura2903@outlook.com>
2026-02-02 16:25:23 +0100

    Oef 4 klaar

---
+++ H10-TicksSince2000/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace H10_TicksSince2000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo belgianCI = new CultureInfo("nl-BE");
+
+            DateTime year2000 = new DateTime(2000, 1, 1);
+            DateTime now = DateTime.Now;
+
+            TimeSpan diff = now - year2000;
+
+            Console.WriteLine($"Sinds {year2000.ToString("d MMMM yyyy", belgianCI)} zijn er {diff.Ticks} ticks voorbijgegaan");
+        }
+    }
+}
