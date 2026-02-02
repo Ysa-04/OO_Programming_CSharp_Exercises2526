844bcb34895c9aa10d0f4686016d4e09015d1f5c
Ysa-04 <ysaura2903@outlook.com>
2026-02-02 16:36:16 +0100

    Oef LeapYearCount

---
+++ H10-LeapYearCount/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace H10_LeapYearCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int leapYearCount = 0;
+
+            for (int index = 1799; index <= 2021; index++)
+            {
+                if (DateTime.IsLeapYear(index))
+                {
+                    leapYearCount++;
+                }
+            }
+            Console.WriteLine($"Er zijn {leapYearCount} schrikkeljaren tussen 1799 en 2021");
+
+            
+        }
+    }
+}
