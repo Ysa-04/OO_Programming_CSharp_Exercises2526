72bead19def7fb0aeac445dc8b4d5dc11b1a8a45
Ysa-04 <ysaura2903@outlook.com>
2026-02-02 14:08:44 +0100

    Add project files.

---
+++ OO_Programming_C#_Exercises2526/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OO_Programming_C__Exercises2526
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
