57922c05e37e8b98d9790f569ddf791170dddc88
Ysa-04 <ysaura2903@outlook.com>
2026-02-26 15:43:41 +0100

    H11_FoodPurchase aangepast

--- H11-FoodPurchase/Program.cs
+++ H11-FoodPurchase/Program.cs
@@ -3,15 +3,21 @@
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            DemoPurchase();
+        {
+            FoodPurchase kaas = new FoodPurchase("Kaas", 2, 2.45, true);
+            FoodPurchase boter = new FoodPurchase("Boter", 0, 5555, true);
+            DemoPurchase(kaas);
+            DemoPurchase(boter);
         }
 
-        public static void DemoPurchase()
+        public static void DemoPurchase(FoodPurchase product)
         {
-            FoodPurchase kaas = new FoodPurchase("Kaas", 1, 4.9, true);
-            Console.WriteLine($"De totaalprijs van {kaas.ProductName} is {kaas.UnitPrice}.");
-            Console.WriteLine($"De vervaldatum van aankoop {kaas.Number} is {kaas.ExprationDate}");
+            
+            if ((product.UnitPrice > 0 || product.UnitPrice < 5000) && product.Number > 0)
+            {
+                Console.WriteLine($"De totaalprijs van {product.ProductName} is {product.CalculateTotalPrice()}.");
+                Console.WriteLine($"De vervaldatum van aankoop {product.Number} is {product.ExprationDate}");
+            } 
         }
     }
 }
