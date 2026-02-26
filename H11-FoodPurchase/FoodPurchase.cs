395d23e6edcca03e8371844b28c739f6115a789c
Ysa-04 <ysaura2903@outlook.com>
2026-02-26 15:33:25 +0100

    H11_FoodPurchase

---
+++ H11-FoodPurchase/FoodPurchase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H11_FoodPurchase
+{
+    internal class FoodPurchase
+    {
+        public bool Refrigerate;
+        private string productName;
+        public string ProductName
+        {
+            get
+            {
+                return this.productName.ToUpper();
+            }
+        }
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    Console.WriteLine("Ongeldig aantal");
+                }
+                else
+                {
+                    number = value;
+                }
+            }
+        }
+        private double unitPrice;
+        public double UnitPrice
+        {
+            get
+            {
+                return unitPrice;
+            }
+            set
+            {
+                if (value < 0 || value >= 5000)
+                {
+                    Console.WriteLine("Ongeldige eenheidsprijs");
+                }
+                else
+                {
+                    unitPrice = value;
+                }
+            }
+        }
+        public DateTime ExprationDate
+        {
+            get
+            {
+                return DateTime.Now.AddMonths(2); //in get niet in set!!!!
+            }
+        }
+
+        public FoodPurchase(string productName, byte number, double unitPrice, bool refrigerate )
+        {
+            this.productName = productName;
+            Number = number;
+            UnitPrice = unitPrice;
+            Refrigerate = refrigerate;
+        }
+
+        public double CalculateTotalPrice()
+        {
+            return this.number * this.unitPrice;
+        }   
+    }
+}
---
+++ H11-FoodPurchase/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace H11_FoodPurchase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            DemoPurchase();
+        }
+
+        public static void DemoPurchase()
+        {
+            FoodPurchase kaas = new FoodPurchase("Kaas", 1, 4.9, true);
+            Console.WriteLine($"De totaalprijs van {kaas.ProductName} is {kaas.UnitPrice}.");
+            Console.WriteLine($"De vervaldatum van aankoop {kaas.Number} is {kaas.ExprationDate}");
+        }
+    }
+}
