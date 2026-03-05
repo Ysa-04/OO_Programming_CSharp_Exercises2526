abada7ce8cdf2533aee58545e6001f6a85d20512
Ysa-04 <ysaura2903@outlook.com>
2026-03-05 15:32:23 +0100

    H13-PhoneBookNameNumber

---
+++ H13-PhoneBookNameNumber/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace H13_PhoneBookNameNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            PhoneBookNumber();
+        }
+        public static void PhoneBookNumber()
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+            Console.WriteLine("Wil je een naam en nnummer inlezen?");
+            string input = Console.ReadLine();
+            bool loop = false;
+            if (input == "ja")
+            {
+                loop = true;
+            }
+            else
+            {
+                loop = false;
+            }
+
+            while (loop)
+            {
+                Console.WriteLine("Naam?");
+                string name = Console.ReadLine();
+                Console.WriteLine("Nummer?");
+                string number = Console.ReadLine();
+
+                if (phoneBook.ContainsKey(name))
+                {
+                    phoneBook[name] = number;
+                }
+                else
+                {
+                    phoneBook.Add(name, number);
+                }
+
+                Console.WriteLine("Wil je nog een naam en nummer inlezen?");
+                input = Console.ReadLine();
+                if (input == "ja")
+                {
+                    loop = true;
+                }
+                else
+                {
+                    loop = false;
+                }
+
+                foreach (var item in phoneBook)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+        }
+    }
+}
